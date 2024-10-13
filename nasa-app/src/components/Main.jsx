9426d53b6e1ee64ba76721e18cd1741d8c7a0c89
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-10-13 14:39:16 -0300

    video type support

--- nasa-app/src/components/Main.jsx
+++ nasa-app/src/components/Main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
+function extractVideoId(url) {
+  console.log("hdurl: " + url)
+  if (!url) return null;
+  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  const matches = url.match(regex);
+  return matches ? matches[1] : null;
+}
+
 export default function Main(props) {
   const {data} = props
   return (
     <div className='imgContainer'>
-      <img src={data?.hdurl} alt={data?.title || 'bg-img-demo'} className='bgImage'/>
+      {data?.media_type === 'image' ? (
+        <img src={data?.hdurl} alt={data?.title || 'bg-img-demo'} className='bgImage' />
+      ) : data?.media_type === 'video' ? (
+        <iframe
+          className='bgImage'
+          src={`https://www.youtube.com/embed/${extractVideoId(data?.url)}?autoplay=1&mute=1`}
+          title={data?.title || 'YouTube video'}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen />
+      ) : (
+        <div className='bgImage'><p>Unsupported media type</p></div>
+      )}
     </div>
   )
 }
