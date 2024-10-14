bce68c9097fce2895c8f42f25a4f4640663aa3ff
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-10-13 21:30:20 -0300

    support any video channel

--- nasa-app/src/components/Main.jsx
+++ nasa-app/src/components/Main.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 function extractVideoId(url) {
   console.log("hdurl: " + url)
   if (!url) return null;
-  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|embed|e(?:mbed)?)\/|watch\?v=|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})|vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/(?:\w+\/)?|album\/(?:\d+\/)?video\/|video\/|)(\d+)|dailymotion\.com\/video\/([a-zA-Z0-9]+)/;
+
   const matches = url.match(regex);
-  return matches ? matches[1] : null;
+  const id = matches ? matches[1] || matches[2] || matches[3] : null;
+  console.log("id: " + id)
+  return id;
 }
 
 export default function Main(props) {
@@ -18,8 +21,8 @@ export default function Main(props) {
       ) : data?.media_type === 'video' ? (
         <iframe
           className='bgImage'
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}`}
-          title={data?.title || 'YouTube video'}
+          src={`${data?.url}&autoplay=1&mute=1&loop=1&playlist=${videoId}`}
+          title={data?.title || 'video-demo'}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         />
